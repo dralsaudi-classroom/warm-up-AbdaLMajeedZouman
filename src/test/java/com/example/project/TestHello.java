5189be262871c0ae12add83fc76165ac980d7cd0
AbdaLMajeedZouman <444101580@student.ksu.edu.sa>
2024-09-04 19:05:11 +0300

    Update TestHello.java

--- src/test/java/com/example/project/TestHello.java
+++ src/test/java/com/example/project/TestHello.java
@@ -22,7 +22,7 @@ public class TestHello {
      Hello.main(null);
 
      // assertion
-     assertEquals("Hello world!\n", bos.toString());
+     assertEquals("Hello world!\nf", bos.toString());
 
      // undo the binding in System
      System.setOut(originalOut);
